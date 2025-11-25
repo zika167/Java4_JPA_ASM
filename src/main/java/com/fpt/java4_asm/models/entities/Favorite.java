bf50c0bcbcf5c883668e4e3497bf0ba55c017cac
Khoa180806 <khoaphan180806@gmail.com>
2025-11-25 20:28:23 +0700

    Finish Entity

--- src/main/java/com/fpt/java4_asm/models/entities/Favorite.java
+++ src/main/java/com/fpt/java4_asm/models/entities/Favorite.java
@@ -1,5 +1,35 @@
 package com.fpt.java4_asm.models.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "favorite")
 public class Favorite {
-        
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    @NotNull(message = "User is required")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "VideoId", nullable = false)
+    @NotNull(message = "Video is required")
+    private Video video;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "LikeDate", nullable = false)
+    private Date likeDate = new Date();
 }
--- src/main/java/com/fpt/java4_asm/models/entities/Video.java
+++ src/main/java/com/fpt/java4_asm/models/entities/Video.java
@@ -1,5 +1,8 @@
 package com.fpt.java4_asm.models.entities;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Video {
         
 }
