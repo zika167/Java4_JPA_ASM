2afbd61d52f001a2d7ddbba18d1e959dd8dfe000
ThienlocTran <tranthienloc21102005@gmail.com>
2025-11-27 10:48:10 +0700

    Create User DTO respone

--- src/main/java/com/fpt/java4_asm/dto/response/UserResponse.java
+++ src/main/java/com/fpt/java4_asm/dto/response/UserResponse.java
@@ -1,5 +1,20 @@
 package com.fpt.java4_asm.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
-        
+    private String fullName;
+    private String email;
+    private String id;
+    private String username;
+    private Date createdDate;
+
+
 }
