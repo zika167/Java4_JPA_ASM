0318cdafe2eb8d918ad300b14b407d03b7d9b463
ThienlocTran <tranthienloc21102005@gmail.com>
2025-11-27 10:33:16 +0700

    Create User entity

--- src/main/java/com/fpt/java4_asm/models/entities/User.java
+++ src/main/java/com/fpt/java4_asm/models/entities/User.java
@@ -3,6 +3,8 @@ package com.fpt.java4_asm.models.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,4 +14,27 @@ public class User {
     @Id
     @Column(name = "Id")
     private String id;
+
+    @Column(name = "Password")
+    private String password;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Fullname")
+    private String fullname;
+
+    @Column(name = "Admin")
+    private Boolean admin;
+
+    @Column(name = "CreatedDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Column(name = "UpdatedDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
+
+    @Transient
+    private String confirmPassword;
 }
