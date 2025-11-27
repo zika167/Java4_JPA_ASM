3a6f45dd7bc4846e57ed1051550b25067e7c3786
ThienlocTran <tranthienloc21102005@gmail.com>
2025-11-27 10:52:12 +0700

    Create UserRepo with 2 method is findByUserName and findByEmail

--- src/main/java/com/fpt/java4_asm/repositories/UserRepo.java
+++ src/main/java/com/fpt/java4_asm/repositories/UserRepo.java
@@ -1,5 +1,12 @@
 package com.fpt.java4_asm.repositories;
 
-public interface UserRepo {
+import com.fpt.java4_asm.models.entities.User;
+
+import java.util.Optional;
+
+public interface UserRepo extends BaseRepository<User,String> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
         
 }
