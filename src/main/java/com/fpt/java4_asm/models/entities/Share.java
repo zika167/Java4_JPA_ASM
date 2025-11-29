ff6d0173aa0bc14a030e2f79813ae47296ec5fa8
SonHoangKimVan <dannn1302@gmail.com>
2025-11-29 20:59:53 +0700

    add basic share video entity and DTO

--- src/main/java/com/fpt/java4_asm/models/entities/Share.java
+++ src/main/java/com/fpt/java4_asm/models/entities/Share.java
@@ -1,5 +1,35 @@
 package com.fpt.java4_asm.models.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Share")
 public class Share {
-        
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "Id")
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "UserId", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "VideoId", nullable = false)
+    private Video video;
+
+    @Column(name = "Emails", columnDefinition = "NVARCHAR(MAX)")
+    private String emails; // Store as JSON array string
+
+    @Column(name = "ShareDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date shareDate = new Date();
 }
